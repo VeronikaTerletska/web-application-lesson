a67495880e06de21d465ff321aa8230ddedc7cbc
VeronikaTerletska <104487544+VeronikaTerletska@users.noreply.github.com>
2023-01-03 16:36:45 +0200

    no render hoe to fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
+import { Routes, Route } from 'react-router-dom';
+import { Home } from 'pages/Home';
+import { About } from 'pages/About';
+import { Products } from 'pages/Products';
+import { ProductDetails } from 'pages/ProductDetails';
+import { Container, Header, Logo, Link } from './App.styled';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <Header>
+        <Logo>
+          <span role="img" aria-label="computer icon"></span> GoMerch Store
+        </Logo>
+      </Header>
+      <nav>
+        <Link to="/" end>
+          Home
+        </Link>
+        <Link to="/about">About</Link>
+        <Link to="/products">Products</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:productId" element={<ProductDetails />} />
+      </Routes>
+    </Container>
   );
 };
