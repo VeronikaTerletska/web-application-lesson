806b09b96a6033c91c452e93122decb4b5a52d9c
VeronikaTerletska <104487544+VeronikaTerletska@users.noreply.github.com>
2023-01-05 11:58:00 +0200

    add basename

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/web-application-lesson">
       <App />
     </BrowserRouter>
   </StrictMode>
